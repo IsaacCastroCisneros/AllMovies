931904c1e2de51e8ae22847dff9f37707ac112fb
isaac <isaac_castro_25@outlook.com>
2022-07-17 08:12:21 -0500

    sin dot

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
 import express from "express";
 import expressLayouts from "express-ejs-layouts";
 import mongoose from "mongoose";
-import dotenv from "dotenv";
+/* import dotenv from "dotenv"; */
 
 import { indexRouter } from "./routes/index.js";
 import { directorsRouter } from "./routes/directors.js";
 
 
-if(process.env.NODE_ENV !== 'production')
+/* if(process.env.NODE_ENV !== 'production')
 {
     dotenv.config();
-}
+} */
 
 const app = express();
-
-app.listen(process.env.PORT || 3000);
 
 app.set('view engine','ejs');
 app.set('views','./views');
@@ -24,11 +22,13 @@ app.use(expressLayouts);
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 
-mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser: true});
+/* mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser: true});
 const db = mongoose.connection;
 db.on('error',error=>console.log('mongodb | an error occurred' + error));
 db.once('open',()=> console.log('mongodb | succesfully connected'));
 
 app.use('/',indexRouter);
-app.use('/directors',directorsRouter);
+app.use('/directors',directorsRouter); */
+
+app.listen(process.env.PORT || 3000);
 
