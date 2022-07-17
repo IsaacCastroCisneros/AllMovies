b5a1eca147258610fabdc2dd7b9d713947ab132f
isaac <isaac_castro_25@outlook.com>
2022-07-17 10:56:04 -0500

    yape

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ import mongoose from "mongoose";
 import { indexRouter } from "./routes/index.js";
 import { directorsRouter } from "./routes/directors.js";
 
-
 import * as dotenv from 'dotenv'
 if(process.env.NODE_ENV !== 'production')
 {
