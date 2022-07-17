64103d9e6f3cad0b1f981e7d912f6641f6356786
isaac <isaac_castro_25@outlook.com>
2022-07-17 10:25:41 -0500

    require

--- server.js
+++ server.js
@@ -1,33 +1,34 @@
-import express from "express";
+const express = require('express');
+/* import express from "express";
 import expressLayouts from "express-ejs-layouts";
 import mongoose from "mongoose";
 
 import { indexRouter } from "./routes/index.js";
 import { directorsRouter } from "./routes/directors.js";
 
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv'; */
 if(process.env.NODE_ENV !== 'production')
 {
-    dotenv.config();
+    require('dotenv').config();
 }
 
 const app = express();
 
-app.set('view engine','ejs');
+/* app.set('view engine','ejs');
 app.set('views','./views');
 app.set('layout', 'layouts/layout');
 
 app.use(expressLayouts);
 app.use(express.static('public'));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true})); */
 
-mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser: true});
+/* mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser: true});
 const db = mongoose.connection;
 db.on('error',error=>console.log('mongodb | an error occurred' + error));
 db.once('open',()=> console.log('mongodb | succesfully connected'));
 
 app.use('/',indexRouter);
-app.use('/directors',directorsRouter);
+app.use('/directors',directorsRouter); */
 
 app.listen(process.env.PORT || 3000);
 
