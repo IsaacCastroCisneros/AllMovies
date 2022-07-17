a85acfec5c4dca4ca2396d0b5fbe3f3a525f0737
isaac <isaac_castro_25@outlook.com>
2022-07-17 08:22:22 -0500

    dot bien puesto

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 import express from "express";
 import expressLayouts from "express-ejs-layouts";
 import mongoose from "mongoose";
-/* import dotenv from "dotenv"; */
+import * as dotenv from "dotenv";
 
 import { indexRouter } from "./routes/index.js";
 import { directorsRouter } from "./routes/directors.js";
 
 
 /* if(process.env.NODE_ENV !== 'production')
-{
+{ */
     dotenv.config();
-} */
+/* } */
 
 const app = express();
 
@@ -22,13 +22,13 @@ app.use(expressLayouts);
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 
-/* mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser: true});
+mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser: true});
 const db = mongoose.connection;
 db.on('error',error=>console.log('mongodb | an error occurred' + error));
 db.once('open',()=> console.log('mongodb | succesfully connected'));
 
 app.use('/',indexRouter);
-app.use('/directors',directorsRouter); */
+app.use('/directors',directorsRouter);
 
 app.listen(process.env.PORT || 3000);
 
