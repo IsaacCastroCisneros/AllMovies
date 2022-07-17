dd7ca01bbe68f2f36cc615174b23d4511f5cc77d
isaac <isaac_castro_25@outlook.com>
2022-07-17 10:55:37 -0500

    yape

--- server.js
+++ server.js
@@ -1,36 +1,34 @@
-/* const express = require('express'); */
-import * as dotenv from 'dotenv'
 import express from "express";
-/* import expressLayouts from "express-ejs-layouts";
+import expressLayouts from "express-ejs-layouts";
 import mongoose from "mongoose";
 
 import { indexRouter } from "./routes/index.js";
-import { directorsRouter } from "./routes/directors.js"; */
+import { directorsRouter } from "./routes/directors.js";
 
 
-/* dotenv.config() */
-/* if(process.env.NODE_ENV !== 'production')
+import * as dotenv from 'dotenv'
+if(process.env.NODE_ENV !== 'production')
 {
    dotenv.config();
-} */
+}
 
 const app = express();
 
-/* app.set('view engine','ejs');
+app.set('view engine','ejs');
 app.set('views','./views');
 app.set('layout', 'layouts/layout');
 
 app.use(expressLayouts);
 app.use(express.static('public'));
-app.use(express.urlencoded({extended: true})); */
+app.use(express.urlencoded({extended: true}));
 
-/* mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser: true});
+mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser: true});
 const db = mongoose.connection;
 db.on('error',error=>console.log('mongodb | an error occurred' + error));
 db.once('open',()=> console.log('mongodb | succesfully connected'));
 
 app.use('/',indexRouter);
-app.use('/directors',directorsRouter); */
+app.use('/directors',directorsRouter);
 
 app.listen(process.env.PORT || 3000);
 
